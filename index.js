1c6455977034d90a8747530e9bfefd47460f66c4
boykopovar <boykopovar@gmail.com>
2025-05-15 18:55:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -118,8 +118,6 @@ function protectMathFormulas(text) {
         .replace(/\\\\\[/g, "{{MATH_DISPLAY_START}}")
         .replace(/\\\\\]/g, "{{MATH_DISPLAY_END}}")
         .replace(/\$\$/g, "{{MATH_DOLLAR}}")
-        .replace(/\\left/g, "{{MATH_LEFT}}")
-        .replace(/\\right/g, "{{MATH_RIGHT}}")
         .replace(/\\,/g, "")
         .replace(/\\int\{([^}]*)\}\{([^}]*)\}/g, "\\int_{$1}^{$2}");
 }
@@ -130,9 +128,7 @@ function restoreMathFormulas(html) {
         .replace(/{{MATH_INLINE_END}}/g, '\\)')
         .replace(/{{MATH_DISPLAY_START}}/g, '\\[')
         .replace(/{{MATH_DISPLAY_END}}/g, '\\]')
-        .replace(/{{MATH_DOLLAR}}/g, '$$')
-        .replace(/{{MATH_LEFT}}/g, "\\left")
-        .replace(/{{MATH_RIGHT}}/g, "\\right");
+        .replace(/{{MATH_DOLLAR}}/g, '$$');
 }
 
 function renderChatMarkdown(chat) {
